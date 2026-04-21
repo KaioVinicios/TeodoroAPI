3493687d34d9842a982c536342357a622ac64957
Wagner <wwagnerrrodriguess@gmail.com>
2026-04-21 16:42:39 -0300

    fix(supply): removendo supplylabel do admin

--- apps/supply/admin.py
+++ apps/supply/admin.py
@@ -1,14 +1,6 @@
 from django.contrib import admin
 
-from apps.supply.models import Supply, SupplyLabel
-
-
-@admin.register(SupplyLabel)
-class SupplyLabelAdmin(admin.ModelAdmin):
-    list_display = ["name", "supply_label_type", "category"]
-    search_fields = ["name", "category"]
-    list_filter = ["supply_label_type"]
-
+from apps.supply.models import Supply
 
 @admin.register(Supply)
 class SupplyAdmin(admin.ModelAdmin):
